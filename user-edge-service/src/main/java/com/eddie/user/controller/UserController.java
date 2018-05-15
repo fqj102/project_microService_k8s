e03e1b77a87eedc0c47737f43b0e88434a4e523b
TaiYi <931305033@qq.com>
2018-05-15 14:55:47 +0800

    Send Verify Code

--- user-edge-service/src/main/java/com/eddie/user/controller/UserController.java
+++ user-edge-service/src/main/java/com/eddie/user/controller/UserController.java
@@ -15,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Random;
+
 @Controller
 public class UserController {
 
@@ -48,6 +50,33 @@ public class UserController {
         return new Response("1000",token);
     }
 
+    @PostMapping("/sendVerifyCode")
+    @ResponseBody
+    public Response sendVerifyCode(@RequestParam("mobile") String mobile ,
+                                   @RequestParam("email") String email){
+        String message = "Verify Code is:";
+        String code = randomCode("0123456789",6);
+        try {
+            boolean result = false;
+            if (StringUtils.isNotBlank(mobile)) {
+                result = serviceProvide.getMessageService().sendMobileMessage(mobile, message+code);
+                redisClient.set(mobile,code);
+            } else if (StringUtils.isNotBlank(email)) {
+                result = serviceProvide.getMessageService().sendEmailMessage(email,message+code);
+                redisClient.set(mobile,code);
+            } else {
+                return Response.MOBILE_OR_PHONE_REQUIRED;
+            }
+
+            if (!result){
+                return Response.SEND_VERIFY_CODE_FILED;
+            }
+        }catch (TException e){
+            return Response.exception(e);
+        }
+        return Response.SUCCESS;
+    }
+
     @PostMapping(value = "/register")
     @ResponseBody
     public Response registerUser(PostMessage message){
@@ -56,16 +85,14 @@ public class UserController {
         if (StringUtils.isBlank(user.getEmail())&&StringUtils.isBlank(user.getMobile())){
             return Response.MOBILE_OR_PHONE_REQUIRED;
         }
-
         BeanUtils.copyProperties(user,userInfo);
         try {
             serviceProvide.getUserService().registerUser(userInfo);
         } catch (TException e) {
-            e.printStackTrace();
-            return new Response("999","Inner Error");
+            return Response.exception(e);
         }
 
-        return new Response("1000","success");
+        return Response.SUCCESS;
     }
 
     private User getJsonUserLoginInfo(String params) {
@@ -84,4 +111,14 @@ public class UserController {
         return user;
     }
 
+    private String randomCode(String s, int size) {
+        StringBuilder result = new StringBuilder(size);
+
+        Random random = new Random();
+        for(int i=0;i<size;i++) {
+            int loc = random.nextInt(s.length());
+            result.append(s.charAt(loc));
+        }
+        return result.toString();
+    }
 }
--- user-edge-service/src/main/java/com/eddie/user/response/Response.java
+++ user-edge-service/src/main/java/com/eddie/user/response/Response.java
@@ -8,6 +8,11 @@ public class Response implements Serializable {
 
     private String message;
 
+    public Response(){
+        this.code = "0";
+        this.message = "success";
+    }
+
     public Response(String code, String message) {
         this.code = code;
         this.message = message;
@@ -15,7 +20,15 @@ public class Response implements Serializable {
 
     public static final Response USERNAME_PASSWORD_INVALID = new Response("1001","username or password invalid");
 
-    public static final Response MOBILE_OR_PHONE_REQUIRED = new Response("1001","mobile or phone are required");
+    public static final Response MOBILE_OR_PHONE_REQUIRED = new Response("1002","mobile or phone are required");
+
+    public static final Response SEND_VERIFY_CODE_FILED = new Response("1003","send verify code filed");
+
+    public static final Response SUCCESS = new Response();
+
+    public static final Response exception(Exception e){
+        return new Response("9999",e.getMessage());
+    }
 
     public String getCode() {
         return code;
