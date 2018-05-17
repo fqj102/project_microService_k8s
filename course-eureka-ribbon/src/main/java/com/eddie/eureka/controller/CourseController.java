20c29a8fe8f5dc4c7a5ee00ba7eb5dea0b35b858
TaiYi <931305033@qq.com>
2018-05-17 17:01:03 +0800

    Ribbon Init

---
+++ course-eureka-ribbon/src/main/java/com/eddie/eureka/controller/CourseController.java
@@ -0,0 +1,19 @@
+package com.eddie.eureka.controller;
+
+import com.eddie.eureka.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping(value = "/list")
+    public String hi(){
+        return courseService.getList();
+    }
+
+}
---
+++ course-eureka-ribbon/src/main/java/com/eddie/eureka/service/CourseService.java
@@ -0,0 +1,16 @@
+package com.eddie.eureka.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String getList() {
+        return restTemplate.getForObject("http://COURSE-PROVIDE/getList",String.class);
+    }
+}
